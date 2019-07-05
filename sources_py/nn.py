34e051854ba116c708a6021864058f477d9e5de7
katomasahiro10 <katomasahiro10@gmail.com>
2019-07-05 23:25:23 +0900

    edit functions

--- sources_py/nn.py
+++ sources_py/nn.py
@@ -17,26 +17,16 @@ class Neuron:
         self.activation = 0.0
         self.moduration = 0.0
 
+    def set_activation(self,input_val):
+        if self.neuron_type != 1:
+            raise Exception('Error you are setting activation of non input neuron(at Neuron.set_activation() ) ')
+        self.activation = input_val
+
 class NeuralNetwork:
     def __init__(self):
         self.num_of_neuron = 0
         self.neurons = []
-        self.moduration_vector = []
         self.connections = np.zeros((self.num_of_neuron, self.num_of_neuron))
-
-    def push_neuron(self, neuron):
-        if(not type(neuron) == Neuron):
-            raise Exception('Type error at NeuralNetwork.push_neuron()')
-        self.num_of_neuron += 1
-        self.neurons.append(neuron)
-        connections_list = self.connections.tolist()
-        connections_list.append( [random.uniform(WEIGHT_LOWER_LIMIT, WEIGHT_UPPER_LIMIT) for i in range(self.num_of_neuron-1) ])
-        for i in range(self.num_of_neuron):
-            connections_list[i].append( random.uniform(WEIGHT_LOWER_LIMIT, WEIGHT_UPPER_LIMIT) )
-        self.connections = np.array(connections_list)
-
-    def del_neuron(self, idx):
-        pass
 
     @property
     def activation_vector(self):
@@ -52,16 +42,26 @@ class NeuralNetwork:
             moduration_vector.append(self.neurons[i].moduration)
         return moduration_vector
 
-if __name__=='__main__':
-    n1 = Neuron( NeuronType.INPUT )
-    n2 = Neuron( NeuronType.MODURATION)
-    n3 = Neuron( NeuronType.MODURATION)
-    print(n1.neuron_type, n1.bias, n1.activation, n1.moduration)
-    print(n1.neuron_type.name)
+    def push_neuron(self, neuron):
+        if(not type(neuron) == Neuron):
+            raise Exception('Type error at NeuralNetwork.push_neuron()')
+        self.num_of_neuron += 1
+        self.neurons.append(neuron)
+        connections_list = self.connections.tolist()
+        connections_list.append( [random.uniform(WEIGHT_LOWER_LIMIT, WEIGHT_UPPER_LIMIT) for i in range(self.num_of_neuron-1) ])
+        for i in range(self.num_of_neuron):
+            connections_list[i].append( random.uniform(WEIGHT_LOWER_LIMIT, WEIGHT_UPPER_LIMIT) )
+        self.connections = np.array(connections_list)
 
-    nn1 = NeuralNetwork()
-    nn1.push_neuron(n1)
-    nn1.push_neuron(n2)
-    nn1.push_neuron(n3)
-    print(nn1.num_of_neuron)
-    print(nn1.activation_vector)
+    def pop_neuron(self, idx):
+        pass
+
+    def update_activation(self):
+        pass
+
+if __name__=='__main__':
+    nn = NeuralNetwork()
+    nn.push_neuron(Neuron(NeuronType.INPUT))
+    nn.push_neuron(Neuron(NeuronType.HIDDEN))
+    nn.push_neuron(Neuron(NeuronType.OUTPUT))
+    nn.push_neuron(Neuron(NeuronType.MODURATION))
