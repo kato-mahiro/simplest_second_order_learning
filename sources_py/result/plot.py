8bc024485e74b733891e06ba04147d5e7a827ea5
katomasahiro10 <katomasahiro10@gmail.com>
2019-07-10 18:04:37 +0900

    plot average line

--- sources_py/result/plot.py
+++ sources_py/result/plot.py
@@ -32,13 +32,23 @@ for i in range(1000):
     max_lines[i] = float(max_lines[i])
     ave_lines[i] = float(ave_lines[i])
 
+max_ave_line = [sum(max_lines)/len(max_lines) for i in range(len(max_lines))]
+ave_ave_line = [sum(ave_lines)/len(ave_lines) for i in range(len(ave_lines))]
+
 plt.title(args[1])
 plt.ylim(0.0,1.0)
+plt.xlim(0,1010)
 plt.yticks([0.0,0.25,0.50,0.75,1.0])
 plt.grid(True)
 plt.xlabel('Generation')
 plt.ylabel('Fitness')
 plt.plot(x,max_lines,label='max')
+plt.plot(x,max_ave_line,label='average of best fitness')
+print('max-ave:',sum(max_lines)/len(max_lines))
+print('max-max:',max(max_lines))
 plt.plot(x,ave_lines,label='average')
+plt.plot(x,ave_ave_line,label='average of average fitness')
+print('ave-ave:',sum(ave_lines)/len(ave_lines))
+print('ave-max:',max(ave_lines))
 plt.legend()
 plt.show()
