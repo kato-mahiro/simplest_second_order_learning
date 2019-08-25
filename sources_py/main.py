d3fc807f7c838b1b6f2f39f035461b3e5f769b60
katomasahiro10 <katomasahiro10@gmail.com>
2019-08-25 18:52:52 +0900

    editing

--- sources_py/main.py
+++ sources_py/main.py
@@ -27,9 +27,21 @@ class Agent:
         self.nn.connections = self.original_connections
         self.nn.mask_array = self.original_mask_array
 
+    def make_mutation(self):
+        for r in range(self.nn.num_of_neuron):
+            for c in range(self.nn.num_of_neuron):
+                self.nn.connections[r][c] = \
+                self.nn.mask_array[r][c] = \
+                if(random.random() < MUTATION_PROB):
+                    self.nn.connections[r][c] += random.uniform(-0.1,0.1)
+                    self.nn.mask_array[r][c] = random.choice([0,1])
+            if(random.random() < MUTATION_PROB):
+            self.nn.neurons[r].bias += random.uniform(-0.1,0.1)
+
 if __name__=='__main__':
     agents = [Agent() for i in range(POPULATION_NUM)]
-    task = Task_1(g_num)
+    for g_num in range(GENERATION_NUM):
+        task = Task_1(g_num)
 
         for a_num in range(POPULATION_NUM):
             for l_num in range(LIFETIME_NUM):
@@ -43,8 +55,7 @@ if __name__=='__main__':
                 else:
                     agents[a_num].nn.get_output(task.feedback(False))
             agents[a_num].fitness = agents[a_num].num_correct_answer / LIFETIME_NUM
-            agents[a_num].num_correct_answer = 0
-            agents[a_num].nn.connections = copy.deepcopy(agents[a_num].initial_connections)
+            agents[a_num].make_initial_state()
 
         # evolution
         next_agents=[]
@@ -54,9 +65,10 @@ if __name__=='__main__':
             fitness_list.append(agents[i].fitness)
         print('generation: ',g_num+1)
         print('max: ',fitness_list[0])
+        print('min: ',fitness_list[-1])
         print('ave: ',sum(fitness_list)/len(fitness_list))
-        next_agents = copy.deepcopy(agents[0:4]) #エリート選択
-        for i in range(POPULATION_NUM - 4):
+        next_agents = copy.deepcopy(agents[0:ELITE_NUM]) #エリート選択
+        for i in range(POPULATION_NUM - ELITE_NUM):
             parent_A = random.choices(agents, weights = fitness_list)[0]
             parent_B = random.choices(agents, weights = fitness_list)[0]
             new_Agent = copy.deepcopy(parent_A)
@@ -64,15 +76,12 @@ if __name__=='__main__':
                 for c in range(new_Agent.nn.num_of_neuron):
                     new_Agent.nn.connections[r][c] = \
                     random.choice([ parent_A.nn.connections[r][c], parent_B.nn.connections[r][c] ])
-                    #if(random.random() < 0.001):
-                        #new_Agent.nn.connections[r][c] += random.uniform(-0.1,0.1)
+                    new_Agent.nn.mask_array[r][c] = \
+                    random.choice([ parent_A.nn.mask_array[r][c], parent_B.nn.mask_array[r][c] ])
+
                 new_Agent.nn.neurons[r].bias = \
                 random.choice( [ parent_A.nn.neurons[r].bias, parent_B.nn.neurons[r].bias ])
-                #if(random.random() < 0.001):
-                    #new_Agent.nn.neurons[r].bias += random.uniform(-0.1,0.1)
-                #new_Agent.nn.A = random.choice([ parent_A.nn.A ,parent_B.nn.A ])
-                #new_Agent.nn.B = random.choice([ parent_A.nn.B ,parent_B.nn.B ])
-                #new_Agent.nn.C = random.choice([ parent_A.nn.C ,parent_B.nn.C ])
-                #new_Agent.nn.D = random.choice([ parent_A.nn.D ,parent_B.nn.D ])
+
+            new_Agent.make_mutation()
             next_agents.append(copy.deepcopy(new_Agent))
         agents = next_agents
