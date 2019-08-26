8f07780009dd472ce5511f8a1917fed17fc7968b
katomasahiro10 <katomasahiro10@gmail.com>
2019-08-26 16:34:21 +0900

    modify main.py
    agent.pyを用いるようにした

--- sources_py/main.py
+++ sources_py/main.py
@@ -7,9 +7,9 @@ from task import *
 from agent import *
 
 if __name__=='__main__':
-    agents = [Agent() for i in range(POPULATION_NUM)]
+    agents = [ModulatedHebbianNetworkAgent_1() for i in range(POPULATION_NUM)]
     for g_num in range(GENERATION_NUM):
-        task = Task_1(g_num)
+        task = Task_7(g_num)
 
         for a_num in range(POPULATION_NUM):
             for l_num in range(LIFETIME_NUM):
@@ -23,7 +23,7 @@ if __name__=='__main__':
                 else:
                     agents[a_num].nn.get_output(task.feedback(False))
             agents[a_num].fitness = agents[a_num].num_correct_answer / LIFETIME_NUM
-            agents[a_num].make_initial_state()
+            agents[a_num].revert_to_initial_state()
 
         # evolution
         next_agents=[]
@@ -31,25 +31,17 @@ if __name__=='__main__':
         agents = sorted(agents, key=attrgetter('fitness'), reverse=True)
         for i in range(POPULATION_NUM):
             fitness_list.append(agents[i].fitness)
+
         print('generation: ',g_num+1)
         print('max: ',fitness_list[0])
         print('min: ',fitness_list[-1])
         print('ave: ',sum(fitness_list)/len(fitness_list))
+
         next_agents = copy.deepcopy(agents[0:ELITE_NUM]) #エリート選択
         for i in range(POPULATION_NUM - ELITE_NUM):
             parent_A = random.choices(agents, weights = fitness_list)[0]
             parent_B = random.choices(agents, weights = fitness_list)[0]
-            new_Agent = copy.deepcopy(parent_A)
-            for r in range(new_Agent.nn.num_of_neuron):
-                for c in range(new_Agent.nn.num_of_neuron):
-                    new_Agent.nn.connections[r][c] = \
-                    random.choice([ parent_A.nn.connections[r][c], parent_B.nn.connections[r][c] ])
-                    new_Agent.nn.mask_array[r][c] = \
-                    random.choice([ parent_A.nn.mask_array[r][c], parent_B.nn.mask_array[r][c] ])
-
-                new_Agent.nn.neurons[r].bias = \
-                random.choice( [ parent_A.nn.neurons[r].bias, parent_B.nn.neurons[r].bias ])
-
-            new_Agent.make_mutation()
-            next_agents.append(copy.deepcopy(new_Agent))
+            new_agent = parent_A.crossover(parent_B)
+            new_agent.mutate()
+            next_agents.append(new_agent)
         agents = next_agents
