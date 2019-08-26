8804bffa30c74877b6e5b76032af3ba8c235e5d1
katomasahiro10 <katomasahiro10@gmail.com>
2019-08-26 14:59:05 +0900

    remove Agent class definition

--- sources_py/main.py
+++ sources_py/main.py
@@ -4,39 +4,7 @@ import sys
 from const import *
 from nn import *
 from task import *
-
-class Agent:
-    def __init__(self):
-        self.nn = NeuralNetwork()
-        self.nn.push_neuron(Neuron(NeuronType.INPUT))
-        self.nn.push_neuron(Neuron(NeuronType.INPUT))
-        self.nn.push_neuron(Neuron(NeuronType.INPUT))
-        self.nn.push_neuron(Neuron(NeuronType.INPUT))
-        self.nn.push_neuron(Neuron(NeuronType.OUTPUT))
-        self.nn.push_neuron(Neuron(NeuronType.OUTPUT))
-        self.nn.push_neuron(Neuron(NeuronType.HIDDEN))
-        self.nn.push_neuron(Neuron(NeuronType.HIDDEN))
-
-        self.original_connections = copy.deepcopy(self.nn.connections)
-        self.original_mask_array = copy.deepcopy(self.nn.mask_array)
-
-        self.num_correct_answer = 0
-        self.fitness = 0.0
-
-    def make_initial_state(self):
-        self.nn.connections = self.original_connections
-        self.nn.mask_array = self.original_mask_array
-
-    def make_mutation(self):
-        for r in range(self.nn.num_of_neuron):
-            for c in range(self.nn.num_of_neuron):
-                self.nn.connections[r][c] = \
-                self.nn.mask_array[r][c] = \
-                if(random.random() < MUTATION_PROB):
-                    self.nn.connections[r][c] += random.uniform(-0.1,0.1)
-                    self.nn.mask_array[r][c] = random.choice([0,1])
-            if(random.random() < MUTATION_PROB):
-            self.nn.neurons[r].bias += random.uniform(-0.1,0.1)
+from agent import *
 
 if __name__=='__main__':
     agents = [Agent() for i in range(POPULATION_NUM)]
