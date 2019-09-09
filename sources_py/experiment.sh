f65b3c33a7a75f8c7f460bf96747d1c75054dee4
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-09 09:15:11 +0900

    実験用設定変更

--- sources_py/experiment.sh
+++ sources_py/experiment.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-python main.py N 8 > ./result/Task8_Normal
-python main.py H 8 > ./result/Task8_Hebbian
-python main.py M1H 8 > ./result/Task8_M1_Hebbian
-python main.py M2H 8 > ./result/Task8_M2_Hebbian
-python main.py E 8 > ./result/Task8_Extended_Hebbian
+python main.py FH 1 > ./result/Task_1_FH
+echo '1/4'
+python main.py FEH 1 > ./result/Task_1_FEH
+echo '2/4'
+python main.py FH 8 > ./result/Task_8_FH
+echo '3/4'
+python main.py FEH 8 > ./result/Task_8_FEH
+echo '4/4'
+echo  
+echo  
+echo  
 echo 'completed!';
