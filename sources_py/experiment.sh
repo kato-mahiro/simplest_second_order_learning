8979128c4b4e5d0def2ed7ba92b495846bf496b3
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-11 21:09:46 +0900

    modify experiment.sh

--- sources_py/experiment.sh
+++ sources_py/experiment.sh
@@ -7,9 +7,9 @@ python main.py M1EH 1 > ./result/Task_1_M1EH
 python main.py FH 2 > ./result/Task_2_FH
 python main.py FEH 2 > ./result/Task_2_FEH
 python main.py N 2 > ./result/Task_2_N
-python main.py M1EH 2 > ./result/Task_2_M2EH
+python main.py M1EH 2 > ./result/Task_2_M1EH
 
 python main.py FH 8 > ./result/Task_8_FH
 python main.py FEH 8 > ./result/Task_8_FEH
 python main.py N 8 > ./result/Task_8_N
-python main.py M1EH 8 > ./result/Task_8_M8EH
+python main.py M1EH 8 > ./result/Task_8_M1EH
