8927349cf66a087f62fc2dd3cf0241940387cb9b
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-11 21:07:04 +0900

    delete result of old experiment

--- sources_py/experiment.sh
+++ sources_py/experiment.sh
@@ -1,21 +1,15 @@
 #!/bin/bash
 python main.py FH 1 > ./result/Task_1_FH
-echo '1'
 python main.py FEH 1 > ./result/Task_1_FEH
-echo '2'
+python main.py N 1 > ./result/Task_1_N
+python main.py M1EH 1 > ./result/Task_1_M1EH
+
 python main.py FH 2 > ./result/Task_2_FH
-echo '3'
 python main.py FEH 2 > ./result/Task_2_FEH
-echo '4'
+python main.py N 2 > ./result/Task_2_N
+python main.py M1EH 2 > ./result/Task_2_M2EH
+
 python main.py FH 8 > ./result/Task_8_FH
-echo '5'
 python main.py FEH 8 > ./result/Task_8_FEH
-echo '6'
-python main.py FH 10 > ./result/Task_10_FH
-echo '7'
-python main.py FEH 10 > ./result/Task_10_FEH
-echo '8'
-echo  
-echo  
-echo  
-echo 'completed!';
+python main.py N 8 > ./result/Task_8_N
+python main.py M1EH 8 > ./result/Task_8_M8EH
