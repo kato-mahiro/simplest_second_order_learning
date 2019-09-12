4cbe172bb79b988d3020490e670c8d3a2f90a2a8
katomasahiro10 <katomasahiro10@gmail.com>
2019-09-12 14:48:13 +0900

    実験用スクリプト

--- sources_py/experiment.sh
+++ sources_py/experiment.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
-python main.py FH 1 > ./result/Task_1_FH
-python main.py FEH 1 > ./result/Task_1_FEH
-python main.py N 1 > ./result/Task_1_N
-python main.py M1EH 1 > ./result/Task_1_M1EH
-
-python main.py FH 2 > ./result/Task_2_FH
-python main.py FEH 2 > ./result/Task_2_FEH
-python main.py N 2 > ./result/Task_2_N
-python main.py M1EH 2 > ./result/Task_2_M1EH
-
-python main.py FH 8 > ./result/Task_8_FH
 python main.py FEH 8 > ./result/Task_8_FEH
-python main.py N 8 > ./result/Task_8_N
 python main.py M1EH 8 > ./result/Task_8_M1EH
+python main.py EH 8 > ./result/Task_8_EH
